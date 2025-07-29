7cdf6d00af008d1a4b91682d9c39dd5fcf0f273f
Jakub Oleksiak <yakubofficialmail@gmail.com>
2025-07-29 07:01:36 +0200

    Dotfiles updated for GNU Stow
    I started managing my dotfiles using GNU Stow. That means it's much simpler to manage dotfiles with the cost of changing the naming convention of the whole repository (little price). There're some changes to vim, kitty config, also there's an addition of i3 and zsh config. Additional configuration scripts are located in ./conf directory.

--- conf/conf-vim.sh
+++ conf/conf-vim.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-__sh_config_path="https://raw.githubusercontent.com/itsYakub/dotfiles/refs/heads/main/vim/.vimrc"
 __sh_vimplug_path="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 function sh_check_util {
@@ -16,24 +15,10 @@ function sh_check_util {
 # STEP 1: Check if utilities exists...
 
 sh_check_util git
-sh_check_util wget
 sh_check_util curl
 sh_check_util vim
 
-# STEP 2: Download the config file from github...
-
-echo "[ $(date +%H:%M:%S) ] wget: processing $__sh_config_path..."
-
-wget -qO - $__sh_config_path > $HOME/.vimrc
-if [ "$(echo $?)" -eq 0 ];
-then
-	echo "[ $(date +%H:%M:%S) ] wget: success"
-else
-	echo "[ $(date +%H:%M:%S) ] wget: failure"
-	exit 1
-fi;
-
-# STEP 3: Clone vim-plug...
+# STEP 2: Clone vim-plug...
 
 echo "[ $(date +%H:%M:%S) ] curl: processing $__sh_vimplug_path..."
 
@@ -46,7 +31,7 @@ else
 	exit 1
 fi;
 
-# STEP 4: Configure vim with vim-plug
+# STEP 3: Configure vim with vim-plug
 
 echo "[ $(date +%H:%M:%S) ] vim: settig up the environment"
 
